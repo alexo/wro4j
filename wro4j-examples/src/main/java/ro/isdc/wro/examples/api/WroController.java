67bc0b51c181de19db6b99d2a91b0d5b80adf0bf
Alex Objelean <alex.objelean@gmail.com>
2011-03-16 00:09:19 +0200

    small updates

--- wro4j-examples/src/main/java/ro/isdc/wro/examples/api/WroController.java
+++ wro4j-examples/src/main/java/ro/isdc/wro/examples/api/WroController.java
@@ -6,6 +6,7 @@ package ro.isdc.wro.examples.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -19,6 +20,13 @@ public class WroController {
   @RequestMapping(method = RequestMethod.GET)
   @ResponseBody
   public String processSubmit() {
+    return "Hello world";
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  @ResponseBody
+  // js_code or code_url
+  public String process(@RequestParam(value = "code_url") final String codeUrl) {
     return "Hello world";
   }
 }
