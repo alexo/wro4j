2a59241b641f4e08b87bfe51a610c34a1e631bc1
Alex Objelean <alex.objelean@gmail.com>
2013-01-18 00:42:17 +0200

    configuration updates

--- wro4j-core/src/test/scala/ro/isdc/wro/TestWroRuntimeException.scala
+++ wro4j-core/src/test/scala/ro/isdc/wro/TestWroRuntimeException.scala
@@ -3,18 +3,21 @@ package ro.isdc.wro
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.specs2.mutable.Specification
+import org.specs2.mutable.SpecificationFeatures
 
 import ro.isdc.wro.config.Context
 
 class TestWroRuntimeException {
   var victim: WroRuntimeException = null
-
   @Before def setUp {
     Context.set(Context.standaloneContext())
   }
-
   @After def tearDown = Context.unset()
 
-  @Test def test = print("first test")
+  @Test def test = {
+    print("first test")
+    //throw new RuntimeException("oops")
+  }
 
 }
